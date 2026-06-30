25bcb80216233d863947f3dafe4792142f90857b
Suman Seth <sethn533@gmail.com>
2026-06-30 09:46:52 +0530

    Add files via upload

--- Langchain/Chat Model/Mistral.py
+++ Langchain/Chat Model/Mistral.py
@@ -6,7 +6,7 @@ load_dotenv()
 api = os.getenv("MISTRAL_API_KEY")
 if not api:
     raise ValueError("MISTRAL_API_KEY not found")
-model = init_chat_model("mistral-small-2506", api_key=api,temperature = 1 )  # 0 < temperature < 1  ----->  High tempareture means creativity and tempareture means logic  
+model = init_chat_model("mistral-small-2506", api_key=api,temperature = 0, max_tokens = 20 )  # 0 < temperature < 1  ----->  High tempareture means creativity and tempareture means logic . max_tokens = 20 means the maximum number of tokens/words/Sentence(dependent on the model) to generate in the chat response.
 result = model.invoke("What is the capital of France?")
 print(result.content)
 
